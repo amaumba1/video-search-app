73c8d3e5877d6d1cac12cbb9365afda4c4c64994
Ally Maumba <amaumba1@gmail.com>
2020-09-22 16:36:54 -0500

    modified the default search and the videolist rendering

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,7 @@ import './VideoItem.css'
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    // Add Event handlers to when user click on the VideoItem, you gonna call a onVideoSelect with the attached video property, we don't want to just invoke onVideoSelect , we want to call it with the video that was passed on VideoItem component. So we will put a arrow function bcoz we want to cal onVideoSelect with some arguments 
+    // Add Event handlers to when user clicks on the VideoItem, you gonna call onVideoSelect with the attached video property, we don't want to just invoke the onVideoSelect , we want to call it with the video that was passed on VideoItem component. So we will put an arrow function bcoz we want to call onVideoSelect with some arguments 
     <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} /> 
         <div className="content">
